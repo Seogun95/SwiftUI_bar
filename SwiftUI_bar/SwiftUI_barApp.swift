89b2449d0c7fe4b1f57f835ebc51086f6bc9c34f
SeonJungKim <ksj083895@gmail.com>
2021-03-17 22:23:31 +0500

    Initial Commit

---
+++ SwiftUI_bar/SwiftUI_barApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_barApp.swift
+//  SwiftUI_bar
+//
+//  Created by 김선중 on 2021/03/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_barApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
